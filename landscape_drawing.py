fa94d7c06083978badee532eca73e00e3ba3a938
Daria Biryukova <biryukovadi@gmail.com>
2018-06-20 14:24:06 +0100

    drawing earth and sky on a window

---
+++ landscape_drawing.py
@@ -0,0 +1,31 @@
+import graphics as gr
+
+def main():
+    window = gr.GraphWin('MyImage', 600, 600)
+    draw_image(window)
+    window.getMouse()
+
+def draw_image(window):
+    house_x, house_y = window.width // 2, window.height * 2 // 3
+    house_width = window.width // 3
+    house_height = house_width * 4 // 3
+    draw_background(window)
+    draw_house(window, house_x, house_y, house_width, house_height)
+
+def draw_background(window):
+    earth = gr.Rectangle(gr.Point(0, window.height // 2),
+                         gr.Point(window.width - 1, window.height - 1))
+    earth.setFill('green')
+    earth.draw(window)
+
+    sky = gr.Rectangle(gr.Point(0,0),
+                       gr.Point(window.width - 1, window.height // 2))
+    sky.setFill('blue')
+    sky.draw(window)
+
+def draw_house(window, house_x, house_y, house_width, house_height):
+    pass # TODO
+
+if __name__ == "__main__":
+    main()
+
