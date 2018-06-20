34b73ae79cd19cad62ad0b1ec65fa1f8fb259948
Daria Biryukova <biryukovadi@gmail.com>
2018-06-20 15:01:34 +0100

    complete house drawing

--- landscape_drawing.py
+++ landscape_drawing.py
@@ -23,8 +23,49 @@ def draw_background(window):
     sky.setFill('blue')
     sky.draw(window)
 
-def draw_house(window, house_x, house_y, house_width, house_height):
-    pass # TODO
+def draw_house(window, x, y, width, height):
+    foundation_height = height // 8
+    walls_height = height // 2
+    walls_width = 7 * width // 8
+    roof_height = height - walls_height - foundation_height
+    draw_house_foundation(window, x, y, width, foundation_height)
+    draw_house_walls(window, x, y - foundation_height, walls_width, walls_height)
+    draw_house_roof(window, x, y - walls_height - foundation_height, width, roof_height)
+    draw_house_window(window, x, y - walls_height // 2, walls_width // 2, walls_height // 2)
+
+def draw_house_foundation(window, x, y, width, heigth):
+    foundation = gr.Rectangle(gr.Point(x - width // 2, y),
+                              gr.Point(x + width // 2, y - heigth))
+    foundation.setFill('brown')
+    foundation.draw(window)
+
+def draw_house_walls(window, x, y, width, heigth):
+    walls = gr.Rectangle(gr.Point(x - width // 2, y),
+                         gr.Point(x + width // 2, y - heigth))
+    walls.setFill('red')
+    walls.draw(window)
+
+def draw_house_roof(window, x, y, width, heigth):
+    roof = gr.Polygon(gr.Point(x - width // 2, y),
+                      gr.Point(x + width // 2, y),
+                      gr.Point(x, y - heigth))
+    roof.setFill('green')
+    roof.draw(window)
+
+def draw_house_window(window, x, y, width, height):
+    glass = gr.Rectangle(gr.Point(x - width // 2, y),
+                         gr.Point(x + width // 2, y - height))
+    glass.setFill("blue")
+    line1 = gr.Line(gr.Point(x, y), gr.Point(x, y - height))
+    line2 = gr.Line(gr.Point(x - width // 2, y - height // 2),
+                    gr.Point(x + width // 2, y - height // 2))
+    glass.draw(window)
+    line1.draw(window)
+    line2.draw(window)
+    line1.setOutline("black")
+    line2.setOutline("black")
+    line1.setWidth(2)
+    line2.setWidth(2)
 
 if __name__ == "__main__":
     main()
